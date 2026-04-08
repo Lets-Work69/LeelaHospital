e0eaaaa66f82589d1044fbe4047d470c20219a24
Disha455 <disha4am.hp@gmail.com>
2026-04-08 19:32:09 +0530

    videoTestimonials Updated

--- frontend/src/assets/videoTestimonials.js
+++ frontend/src/assets/videoTestimonials.js
@@ -1,11 +1,7 @@
 const videoTestimonials = [
+ 
   {
     name: "Patient Video 1",
-    embedUrl: "https://www.instagram.com/reel/DOPAH6vk5Lb/embed"
-  },
-
-  {
-    name: "Patient Video 2",
     videoSrc: "/public/videos/v1.mp4"
   },
   {
