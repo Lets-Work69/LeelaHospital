e74aeccd107ae67148c622e61aee285deb1ac6df
Aditya <mauryaaditya02003@gmail.com>
2026-04-10 16:06:31 +0530

    video dashborad fix

--- frontend/src/assets/videoTestimonials.js
+++ frontend/src/assets/videoTestimonials.js
@@ -2,11 +2,11 @@ const videoTestimonials = [
  
   {
     name: "Patient Video 1",
-    videoSrc: "/public/videos/v1.mp4"
+    videoSrc: "/videos/v1.mp4"
   },
   {
     name: "Patient Video 2",
-    videoSrc: "/public/videos/v2.mp4"
+    videoSrc: "/videos/v2.mp4"
   }
   
  
